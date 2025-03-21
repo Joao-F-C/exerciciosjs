070c15cd0eae9669e7602442519f36c092675953
Joao-F-C <camargos.ferreira@aluno.ifsp.edu.br>
2025-03-21 11:18:30 -0300

    Update exercicio_github.js

--- exercicio_github.js
+++ exercicio_github.js
@@ -20,3 +20,32 @@ if(((nota1+nota2+nota3)/3) > 7){
 }
 
 //3
+let chuva = true
+if (chuva = true) {
+console.log("Precisa de guarda-chuva");
+}
+else {
+  console.log("Não precisa de guarda-chuva");
+}
+
+//4
+let num= 5
+console.log(num*2)
+
+
+
+
+//5
+let idade= 23
+if(idade>=18){
+  console.log("Você é maior de idade")
+} else{
+  console.log("Você é menor de idade")
+}
+
+
+//6
+const saudacao = function(nome){
+  console.log(`Olá, ${nome}, como você está hoje?`);
+};
+saudacao("Ana");
